dd51c1a479d6d6dce43f310d87a2b9541831f941
fernanda-palacios <fernanda.palaciosaltamirano@mail.utoronto.ca>
2021-08-19 13:05:51 -0500

    add email icon copy functionality

--- src/components/SocialTwo.jsx
+++ src/components/SocialTwo.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 
 const SocialShare = [ 
   {
@@ -11,22 +11,41 @@ const SocialShare = [
   },
   {
     iconName: "email",
-    link: "mailto:fernanda.palaciosaltamirano@mail.utoronto.ca"
+    link: "fernanda.palaciosaltamirano@mail.utoronto.ca"
   }
 ];
 const Social = () => {
+
+  const [showCopiedMessage, setShowCopiedMessage] = useState(false)
+  
   return (
     <>
       <ul className="social social-default position-relative">
         {SocialShare.map((val, i) => (
           <li key={i}>
+            {val.iconName == "email" ?
+            <>
+              <img
+                className="svg"
+                src={`/img/svg/social/${val.iconName}.svg`}
+                alt="social"
+                style={{cursor: "pointer"}}
+                onClick={()=>{
+                  navigator.clipboard.writeText(val.link)
+                  setShowCopiedMessage(true)
+                  setTimeout(() => setShowCopiedMessage(false) , 2500)
+                }}
+              ></img>
+              {showCopiedMessage && (<span style={{paddingLeft: '10px'}}>Copied!</span>)}
+            </>
+            :
             <a href={`${val.link}`} target="_blank" rel="noreferrer">
               <img
                 className="svg"
                 src={`/img/svg/social/${val.iconName}.svg`}
                 alt="social"
               ></img>
-            </a>
+            </a>}
           </li>
         ))}
       </ul>
