511ebb5ac96a2e1200f75f4f52fe4c984370b529
fernanda-palacios <fernanda.palaciosaltamirano@mail.utoronto.ca>
2021-08-19 12:52:08 -0500

    add social links

--- src/components/SocialTwo.jsx
+++ src/components/SocialTwo.jsx
@@ -1,23 +1,17 @@
 import React from "react";
 
-const SocialShare = [
+const SocialShare = [ 
   {
-    iconName: "facebook",
-    link: "https://www.facebook.com/",
-  },
-  { iconName: "twitter", link: "https://twitter.com/" },
-  {
-    iconName: "instagram",
-    link: "https://www.instagram.com/",
-  },
-  { iconName: "dribbble", link: "https://dribbble.com/" },
-  {
-    iconName: "tik-tok",
-    link: "https://www.tiktok.com/",
+    iconName: "linkedin",
+    link: "https://www.linkedin.com/in/fernanda-palacios-242578138/"
   },
   {
     iconName: "github",
-    link: "https://github.com/"
+    link: "https://github.com/fernanda-palacios"
+  },
+  {
+    iconName: "email",
+    link: "mailto:fernanda.palaciosaltamirano@mail.utoronto.ca"
   }
 ];
 const Social = () => {
